522a2fc0fc03e3a720d6f3cb62f6209d09519e54
RyazanovEugeniy <ryazanov_eugeniy@mail.ru>
2021-08-27 14:21:32 +0600

    Factory Method

--- C#/Visual Studio/Patterns/Creational/FactoryMethod/FactoryMethod/Factories/CombinedFactory.cs
+++ C#/Visual Studio/Patterns/Creational/FactoryMethod/FactoryMethod/Factories/CombinedFactory.cs
@@ -4,5 +4,16 @@ using FactoryMethod.Transport;
 
 namespace FactoryMethod.Factories
 {
+    class CombinedFactory : ITransportFactory
+    {
+        public ITransport Create()
+        {
+            return new RoadTransport();
+        }
 
+        public override string ToString() // реализуем приведение экземпляра класс к строке
+        {
+            return "Компания комбинированного типа";
+        }
+    }
 }
