7ba69a289f579db7ec164a95f64e82e8be11943d
RyazanovEugeniy <ryazanov_eugeniy@mail.ru>
2021-08-28 12:26:45 +0600

    FactoryMethod Refactoring

--- C#/Visual Studio/Patterns/Creational/FactoryMethod/FactoryMethod/ITransportFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FactoryMethod
-{
-    // Интерфейс Фабрики транспорта
-    interface ITransportFactory
-    {
-        // Метод создающий объект транспорта и возвращающий его
-        ITransport Create();
-    }
-}
