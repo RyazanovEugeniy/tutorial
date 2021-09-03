0b35f2c015486f36c7ba63f1e5efa87e9e5fdd8a
RyazanovEugeniy <ryazanov_eugeniy@mail.ru>
2021-09-03 13:52:45 +0600

    Refactoring

--- C#/Visual Studio/Patterns/Structural/Composite/Composite/Program.cs
+++ C#/Visual Studio/Patterns/Structural/Composite/Composite/Program.cs
@@ -2,23 +2,34 @@
 
 namespace Composite
 {
+    // Суть шаблона Компоновщик (Composite) - создание абстрактного класса (может быть интерфейса?),
+    // в данном примере класса Component,
+    // с какими либо общими методами, в данном случае GetInfo,
+    // что позволяет сформировать из объектов наследуемых от Component древовидную структуру.
+    // В дальнейшем это позволяет работать с этой структурой как с единым объектом
+    // Данный пример
     class Program 
     {
         static void Main(string[] args)
         {
+            // Создаем первую ветку, так как она самая первая, то по факту это дерево;)
             Branch tree = new Branch();
+
+            // На ней вырастает первая ветка с двумя листочками
             Branch branch1 = new Branch();
             branch1.Add(new Leaf());
             branch1.Add(new Leaf());
+            tree.Add(branch1);
+
+            // Еще ветка с одним листочком и веткой без листьев
             Branch branch2 = new Branch();
             branch2.Add(new Leaf());
-            tree.Add(branch1);
-            tree.Add(branch2);
             Branch branch3 = new Branch();
             branch2.Add(branch3);
-            branch3.Add(new Leaf());
+            tree.Add(branch2);
 
-            Console.WriteLine($"Composite: {tree.GetInfo()}\n");
+            // Выводим то, что произрастает
+            Console.WriteLine($"Tree: {tree.GetInfo()}\n");
 
             Console.ReadKey();
         }
