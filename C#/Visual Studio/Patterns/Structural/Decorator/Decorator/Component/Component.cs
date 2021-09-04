952070a37f4c51ac19f5c263d2e002019a11da20
RyazanovEugeniy <ryazanov_eugeniy@mail.ru>
2021-09-05 01:22:54 +0600

    Refactoring and add Decorator pattern

---
+++ C#/Visual Studio/Patterns/Structural/Decorator/Decorator/Component/Component.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Decorator
+{
+    public abstract class Component
+    {
+        public abstract string Operation();
+    }
+
+    class ConcreteComponent : Component
+    {
+        public override string Operation()
+        {
+            return "ConcreteComponent";
+        }
+    }
+
+    abstract class Decorator : Component
+    {
+        protected Component _component;
+
+        public Decorator(Component component)
+        {
+            this._component = component;
+        }
+
+        public void SetComponent(Component component)
+        {
+            this._component = component;
+        }
+
+        public override string Operation()
+        {
+            if (this._component != null)
+            {
+                return this._component.Operation();
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+    }
+
+    class ConcreteDecoratorA : Decorator
+    {
+        public ConcreteDecoratorA(Component comp) : base(comp)
+        {
+        }
+
+        public override string Operation()
+        {
+            return $"ConcreteDecoratorA({base.Operation()})";
+        }
+    }
+
+    class ConcreteDecoratorB : Decorator
+    {
+        public ConcreteDecoratorB(Component comp) : base(comp)
+        {
+        }
+
+        public override string Operation()
+        {
+            return $"ConcreteDecoratorB({base.Operation()})";
+        }
+    }
+}
---
+++ C#/Visual Studio/Patterns/Structural/Decorator/Decorator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Decorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcreteComponent simple = new ConcreteComponent();
+
+            Console.WriteLine("RESULT: " + simple.Operation());
+
+            ConcreteDecoratorA decorator1 = new ConcreteDecoratorA(simple);
+            ConcreteDecoratorB decorator2 = new ConcreteDecoratorB(decorator1);
+            Console.WriteLine("RESULT: " + decorator2.Operation());
+
+            Console.ReadKey();
+        }
+    }
+}
