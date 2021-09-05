46df497717636455d1ab520d468fc887fcca04cd
RyazanovEugeniy <ryazanov_eugeniy@mail.ru>
2021-09-06 03:07:59 +0600

    DemoRace2D. Road work

--- Unity/DemoRace2D/Assets/Scripts/MoveAndSpawnRoad.cs
+++ Unity/DemoRace2D/Assets/Scripts/MoveAndSpawnRoad.cs
@@ -1,13 +1,21 @@
 using UnityEngine;
+using UnityEditor;
 
 public class MoveAndSpawnRoad : MonoBehaviour
 {
     public float speed = 1.0f;
-    void FixedUpdate()
+    public GameObject road;
+    void Update()
     {
         transform.Translate(Vector3.down * Time.deltaTime * speed);
 
-        if (transform.position.y < -10)
+        if (transform.position.y < -10.0f)
+        {
+            Instantiate(road, new Vector3(0.0f, 9.31f, 0.0f), Quaternion.identity);
+            road.transform.localScale = new Vector3(0.8f, 1.0f, 1.0f);
+            //transform.position = new Vector3(-0.78f, 8.11f, 0.0f);
+            //EditorApplication.isPaused = true;
             Destroy(gameObject);
+        }
     }
 }
