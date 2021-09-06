9900278ed29c182bfb0c7bf723b17452fb39b7f2
RyazanovEugeniy <ryazanov_eugeniy@mail.ru>
2021-09-06 20:07:44 +0600

    DemoRace2D. Add MainMenu and GameOverMenu.

--- Unity/DemoRace2D/Assets/Scripts/Player.cs
+++ Unity/DemoRace2D/Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
@@ -21,6 +22,7 @@ public class Player : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
+            SceneManager.LoadScene(2);
             Destroy(gameObject);
         }
     }
---
+++ Unity/DemoRace2D/Assets/Scripts/Scenes.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenes : MonoBehaviour
+{
+    public void LoadScene(int sceneNumber)
+    {
+        SceneManager.LoadScene(sceneNumber);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
