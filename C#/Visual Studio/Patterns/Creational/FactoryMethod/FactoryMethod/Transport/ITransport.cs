f558f4370e077c7c873f3f0fb8bedfb91035c45d
RyazanovEugeniy <ryazanov_eugeniy@mail.ru>
2021-09-07 20:54:22 +0600

    Change folders

--- C#/Visual Studio/Patterns/Creational/FactoryMethod/FactoryMethod/Transport/ITransport.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FactoryMethod.Transport
-{
-    // Интерфейс транспорта компании
-    interface ITransport
-    {
-        // Метод доставки транспортом
-        void Deliver();
-    }
-}
