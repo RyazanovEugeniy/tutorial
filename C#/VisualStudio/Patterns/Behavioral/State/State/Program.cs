266af0da370bbdc51506430ac1e613841b149b7e
RyazanovEugeniy <ryazanov_eugeniy@mail.ru>
2021-10-17 21:06:57 +0600

    Refactoring State pattern. Work.

---
+++ C#/VisualStudio/Patterns/Behavioral/State/State/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace State
+{
+    // Суть шаблона State - запихивание state-machine'ы в ООП,
+    // а конкретно создание абстрактного класса состояния класса,
+    // у которого мы реализуем state-машину,
+    // и реализация поведения в производных классах состояния.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создадим животное в состоянии сна
+            var animal = new Animal(new Sleep());
+
+            // Заставим животное поработать
+            animal.DoWork();
+            // Посмотрим его состояние
+            animal.GetState();
+
+            // Заставим животное поработать
+            animal.DoWork();
+            // Посмотрим его состояние
+            animal.GetState();
+
+            Console.ReadKey();
+        }
+    }
+}
