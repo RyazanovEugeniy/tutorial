b2ff6d319b52c2e3dc9adb047ae5b61f03194314
RyazanovEugeniy <ryazanov_eugeniy@mail.ru>
2021-10-18 23:55:31 +0600

    Refactoring Strategy pattern. Work.

---
+++ C#/VisualStudio/Patterns/Behavioral/Strategy/Strategy/Human/Human.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Strategy
+{
+    // Интерфейс профессии человека
+    public interface IProfession
+    {
+        // Метод работы
+        void DoWork();
+    }
+
+    // Класс танцора
+    class Dancer : IProfession
+    {
+        // Который танцует
+        public void DoWork()
+        {
+            Console.WriteLine("Human dancing");
+        }
+    }
+
+    // Класс певца
+    class Singer : IProfession
+    {
+        // Который внезапно поет
+        public void DoWork()
+        {
+            Console.WriteLine("Human singing");
+        }
+    }
+
+    // Класс нашего человека
+    class Human
+    {
+        // Поле в котором мы храним текущую профессию
+        private IProfession profession;
+
+        // Конструктор
+        public Human()
+        { }
+
+        // Метод установки профессии
+        public void SetProfession(IProfession profession)
+        {
+            this.profession = profession;
+            Console.WriteLine($"SetProfession to {this.GetType().Name}: {profession.GetType().Name}");
+        }
+
+        // Метод работы
+        public void DoWork()
+        {
+            // Который проверяет, есть ли профессия. Если есть работаем, если нет, то ничего не делаем.
+            if (profession != null)
+                this.profession.DoWork();
+            else
+                Console.WriteLine($"{this.GetType().Name} have no profession");
+        }
+    }
+}
--- C#/VisualStudio/Patterns/Behavioral/Strategy/Strategy/Program.cs
+++ C#/VisualStudio/Patterns/Behavioral/Strategy/Strategy/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 
-
 namespace Strategy
 {
+    // Суть шаблона стратегия - вынос части кода
+    // в собственные классы, реализующие один интерфейс (абстрактный класс).
+    // Это позволяет увеличить уровень абстракции изначального класса,
+    // а так же дает возможность смены алгоритмов на ходу.
     class Program
     {
         static void Main(string[] args)
         {
+            // Создадим человека
+            var human = new Human();
+            // Попробуем заставить его работать
+            human.DoWork();
+            Console.WriteLine();
+
+            // Дадим ему профессию
+            human.SetProfession(new Dancer());
+            // Заставим работать
+            human.DoWork();
+            Console.WriteLine();
+
+            // Дадим другую профессию
+            human.SetProfession(new Singer());
+            // И снова заставим работать
+            human.DoWork();
+
             Console.ReadKey();
         }
     }
