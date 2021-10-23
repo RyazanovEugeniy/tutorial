5a1fe6d9ebe0e21d0814ecb3297e1f91f1ecb729
RyazanovEugeniy <ryazanov_eugeniy@mail.ru>
2021-10-23 18:38:36 +0600

    Refactoring Command pattern. Work.

---
+++ C#/VisualStudio/Patterns/Behavioral/Command/Command/Command/Command.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Command
+{
+    // Интерфейс команды
+    public interface ICommand
+    {
+        string Execute();
+    }
+
+    // Простая команда, которая реализует метод Execute
+    class SimpleCommand : ICommand
+    {
+        public string Execute() => "SimpleCommand.Execute()";
+    }
+
+    // Составная команда
+    class CompositeCommand : ICommand
+    {
+        // Которая имеет поле солдата
+        private Soldier soldier;
+
+        // Которого она создает в конструкторе
+        public CompositeCommand() => this.soldier = new Soldier();
+
+        // Реализуем метод интерфейса, через солдата
+        public string Execute() => "CompositeCommand." + this.soldier.Execute();
+    }
+
+    // Класс солдата
+    class Soldier
+    {
+        // Метод выполнения команды
+        public string Execute() => "Soldier.Execute()";
+    }
+
+    // Класс командира
+    class Commander
+    {
+        // С двумя полями установленных команд
+        private ICommand command1;
+        private ICommand command2;
+
+        // Метод установки первой команды
+        public void SetCommand1(ICommand command) => this.command1 = command;
+
+        // Метод установки второй команды
+        public void SetCommand2(ICommand command) => this.command2 = command;
+
+        // Метод запускающий последовательное выполнение двух команд
+        public void Execute()
+        {
+            if (this.command1 is ICommand)
+                Console.WriteLine(this.command1.Execute());
+            if (this.command2 is ICommand)
+                Console.WriteLine(this.command2.Execute());
+        }
+    }
+}
--- C#/VisualStudio/Patterns/Behavioral/Command/Command/Program.cs
+++ C#/VisualStudio/Patterns/Behavioral/Command/Command/Program.cs
@@ -2,10 +2,25 @@
 
 namespace Command
 {
+    // Суть шаблона Command - создание классов под команды системы,
+    // что позволяет в дальнейшем работать с каждой из них,
+    // как с отдельным объектом.
+    // То есть открываются возможности создания очереди команд,
+    // откладывания команды и тд.
     class Program
     {
         static void Main(string[] args)
         {
+            // Создаем командира
+            Commander commander = new Commander();
+
+            // Устанавливаем ему две команды, которые он должен исполнять
+            commander.SetCommand1(new SimpleCommand());
+            commander.SetCommand2(new CompositeCommand());
+
+            // Заставляем его их исполнить
+            commander.Execute();
+
             Console.ReadKey();
         }
     }
