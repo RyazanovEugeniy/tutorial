aec913d13a3fffb55319c0fb3262d374df736df4
RyazanovEugeniy <ryazanov_eugeniy@mail.ru>
2021-10-27 14:13:57 +0600

    Working on TemplateMethod pattern. Work.

--- C#/VisualStudio/Patterns/Behavioral/TemplateMethod/TemplateMethod/Program.cs
+++ C#/VisualStudio/Patterns/Behavioral/TemplateMethod/TemplateMethod/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 
-
 namespace TemplateMethod
 {
+    // Суть шаблона TemplateMethod - задание структуры алгоритма
+    // в методе базового класса, заставляющего производные классы
+    // выполнить все шаги данного алгоритма.
     class Program
     {
         static void Main(string[] args)
         {
+            // Создадим человека
+            SmartHuman smartHuman = new SmartHuman();
+            // Выполним его шаблонный метод
+            smartHuman.TemplateMethod();
+
             Console.ReadKey();
         }
     }
---
+++ C#/VisualStudio/Patterns/Behavioral/TemplateMethod/TemplateMethod/Template/Template.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace TemplateMethod
+{
+    // Класс человека
+    abstract class Human
+    {
+        // Создаем структура алгоритма в шаблонном методе
+        public void TemplateMethod()
+        {
+            this.GetInformation();
+            this.MakeDecision();
+            this.Work();
+        }
+
+        // Абстрактные методы,
+        // которые должны быть в реализованы у производных классов.
+        // Метод поиска информации
+        protected abstract void GetInformation();
+
+        // Метод принятия решения
+        protected abstract void MakeDecision();
+
+        // Метод работы
+        protected abstract void Work();
+    }
+
+    class SmartHuman : Human
+    {
+        // Метод поиска информации
+        protected override void GetInformation()
+        {
+            Console.WriteLine("SmartHuman.GetInformation()");
+        }
+
+        // Метод принятия решения
+        protected override void MakeDecision()
+        {
+            Console.WriteLine("SmartHuman.MakeDecision()");
+        }
+
+        // Метод работы
+        protected override void Work()
+        {
+            Console.WriteLine("SmartHuman.Work()");
+        }
+    }
+}
