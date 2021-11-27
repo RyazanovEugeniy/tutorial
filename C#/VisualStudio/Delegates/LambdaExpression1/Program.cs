118abd612f5fd0985673b9bfe85c691f43a0e55a
RyazanovEugeniy <ryazanov_eugeniy@mail.ru>
2021-11-27 16:25:35 +0600

    Some playing with LambdaExpression.

--- C#/VisualStudio/Delegates/LambdaExpression1/Program.cs
+++ C#/VisualStudio/Delegates/LambdaExpression1/Program.cs
@@ -6,18 +6,26 @@ namespace LambdaExpression
 
     class Program
     {
+        public static int Operation1(int x, int y) => x + y;
+
         static void Main(string[] args)
         {
-            Operation operation1 = delegate(int x, int y)
+            Console.WriteLine("Program.Operation1(2, 6) == " + Program.Operation1(2, 6));
+
+            Operation operation2 = delegate(int x, int y)
             {
                 return x + y;
             };
 
-            Console.WriteLine("operation(3, 4) == " + operation1(3, 4));
+            Console.WriteLine("operation2(3, 4) == " + operation2(3, 4));
 
-            Operation operation2 = (x, y) => x + y;
+            Operation operation3 = (x, y) => x + y;
 
-            Console.WriteLine("operation2(3, 4) == " + operation1(5, 2));
+            Console.WriteLine("operation3(5, 2) == " + operation3(5, 2));
+
+            Operation operation4 = Program.Operation1;
+
+            Console.WriteLine("operation4(8, 4) == " + operation4(8, 4));
 
             Console.ReadKey();
         }
