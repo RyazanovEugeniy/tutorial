708922c5a5ed27301e51f1b15d4add7bc3a66734
RyazanovEugeniy <ryazanov_eugeniy@mail.ru>
2021-11-27 16:18:51 +0600

    Works with Delegates project.

---
+++ C#/VisualStudio/Delegates/LambdaExpression1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LambdaExpression
+{
+    delegate int Operation(int x, int y);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operation operation1 = delegate(int x, int y)
+            {
+                return x + y;
+            };
+
+            Console.WriteLine("operation(3, 4) == " + operation1(3, 4));
+
+            Operation operation2 = (x, y) => x + y;
+
+            Console.WriteLine("operation2(3, 4) == " + operation1(5, 2));
+
+            Console.ReadKey();
+        }
+    }
+}
